9d0b834f33b62820e4b97879a1ab20271a349a79
AyaSKT <AyaSKT.bzh@gmail.com>
2025-02-18 10:37:00 +0800

    0.2

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ import time
 import random
 import shutil as st
 
+# 跨平台清屏
+def clear_screen():
+    # Windows系统
+    if os.name == 'nt':
+        os.system('cls')
+    # Unix/Linux/MacOS系统
+    else:
+        os.system('clear')
+
+clear_screen()
+
+print('Mode options:')
+print('1 Keep original files')
+print('2 Delete original files')
+
+
+mode = int(input('Enter the mode: '))
+
+clear_screen()
+
+time_start = time.time()
+
 os.makedirs('JPG', exist_ok=True)
 os.makedirs('NEF', exist_ok=True)
 
@@ -16,11 +38,18 @@ for file in os.listdir(current_dir):
         if file_ext == '.JPG' or file_ext == '.jpg':
             st.copy2(file, 'JPG')
             print('-> JPG')
+            if mode == 2:
+                os.remove(file)
         elif file_ext == '.NEF' or file_ext == '.nef':
             st.copy2(file, 'NEF')
             print('-> NEF')
+            if mode == 2:
+                os.remove(file)
     else:
         print('-> Ignored')
 
-print('\n\n\n Finished')
-os.system('pause')
+time_end = time.time()
+
+
+print(f'\n\n\nFinished in {time_end - time_start} seconds')
+input('Press Enter to exit...')
