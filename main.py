63bbc43af5fd26efe12813302c203e331968132a
AyaSKT <AyaSKT.bzh@gmail.com>
2025-02-18 11:13:01 +0800

    1.0

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 import time
 import random
 import shutil as st
+import tkinter as tk
+from tkinter import filedialog
+
 
 # 跨平台清屏
 def clear_screen():
@@ -20,36 +23,51 @@ print('1 Keep original files')
 print('2 Delete original files')
 
 
-mode = int(input('Enter the mode: '))
+# mode = int(input('Enter the mode: '))
+mode = 1
 
 clear_screen()
 
-time_start = time.time()
+# time_start = time.time()
 
-os.makedirs('JPG', exist_ok=True)
-os.makedirs('NEF', exist_ok=True)
 
-current_dir = os.getcwd()
+root = tk.Tk()
+root.withdraw()
+
+current_dir = filedialog.askdirectory(title="Choose directory")  # 弹出对话框并获取用户选择的文件夹路径
+if not current_dir:
+    print("No directory selected")
+    sys.exit(1)
+
+jpg_dir = os.path.join(current_dir, 'JPG')
+nef_dir = os.path.join(current_dir, 'NEF')
+os.makedirs(jpg_dir, exist_ok=True)
+os.makedirs(nef_dir, exist_ok=True)
+
+# print(jpg_dir)
+# print(nef_dir)
 
 for file in os.listdir(current_dir):
     print(file, end = ' ')
-    if os.path.isfile(file):
+    file_path = os.path.join(current_dir, file)
+    if os.path.isfile(os.path.join(current_dir, file)):
         file_name, file_ext = os.path.splitext(file)
+        
         if file_ext == '.JPG' or file_ext == '.jpg':
-            st.copy2(file, 'JPG')
+            st.copy2(file_path, jpg_dir)
             print('-> JPG')
             if mode == 2:
                 os.remove(file)
         elif file_ext == '.NEF' or file_ext == '.nef':
-            st.copy2(file, 'NEF')
+            st.copy2(file_path, nef_dir)
             print('-> NEF')
             if mode == 2:
                 os.remove(file)
     else:
         print('-> Ignored')
 
-time_end = time.time()
+# time_end = time.time()
 
 
-print(f'\n\n\nFinished in {time_end - time_start} seconds')
+# print(f'\n\n\nFinished in {time_end - time_start} seconds')
 input('Press Enter to exit...')
