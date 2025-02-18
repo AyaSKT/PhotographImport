7000c1661fd148cd2569322376ed519e98264d58
AyaSKT <AyaSKT.bzh@gmail.com>
2025-02-18 19:54:20 +0800

    1.2

--- main.py
+++ main.py
@@ -46,28 +46,43 @@ os.makedirs(nef_dir, exist_ok=True)
 
 # print(jpg_dir)
 # print(nef_dir)
+j_num = 0
+n_num = 0
+i_num = 0
+
 
 for file in os.listdir(current_dir):
     print(file, end = ' ')
     file_path = os.path.join(current_dir, file)
+    file_jpath = os.path.join(jpg_dir, file)
+    file_npath = os.path.join(nef_dir, file)
     if os.path.isfile(os.path.join(current_dir, file)):
         file_name, file_ext = os.path.splitext(file)
-        
-        if file_ext == '.JPG' or file_ext == '.jpg':
+        if os.path.exists(file_jpath) or os.path.exists(file_npath):
+            print('-> Existed and ignored')
+            i_num += 1
+        elif file_ext == '.JPG' or file_ext == '.jpg':
             st.copy2(file_path, jpg_dir)
-            print('-> JPG')
+            j_num += 1
+            print(f'-> {jpg_dir}')
             if mode == 2:
                 os.remove(file)
         elif file_ext == '.NEF' or file_ext == '.nef':
             st.copy2(file_path, nef_dir)
-            print('-> NEF')
+            n_num += 1
+            print(f'-> {nef_dir}')
             if mode == 2:
                 os.remove(file)
+        else:
+            print('-> Ignored')
+            i_num += 1
     else:
         print('-> Ignored')
-
+        i_num += 1
 # time_end = time.time()
 
 
-# print(f'\n\n\nFinished in {time_end - time_start} seconds')
-input('Press Enter to exit...')
+print(f'JPG: {j_num} times, NEF: {n_num} times, Ignored: {i_num} times')
+print(f'\n\n\nFinished')
+
+# input('Press Enter to exit...')
